03df45c0137a10c401e22c5dec6beba1ed7e37b1
Lê Thành Đạt <datthichlaptrinh@gmail.com>
2024-03-18 23:02:02 +0700

    Ecommerce Multi Auth Setup With Breeze

---
+++ routes/admin.php
@@ -0,0 +1,4 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\AdminController;
+Route::get('dashboard', [AdminController::class,'dashboard'])->name('dashboard');
---
+++ routes/vendor.php
@@ -0,0 +1,4 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\VendorController;
+Route::get('dashboard', [VendorController::class,'dashboard'])->name('dashboard');
