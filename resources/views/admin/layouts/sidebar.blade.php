4f4e47dab1de0b74b7b8dbe04193bafb4b75f4bd
Lê Thành Đạt <datthichlaptrinh@gmail.com>
2024-04-01 06:04:50 +0700

    Ecommerce Admin Sidebar Active

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -19,11 +19,12 @@
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-list"></i>
                     <span>Manage Categories</span></a>
                 <ul class="dropdown-menu">
-                    <li class=""><a class="nav-link" href="{{ route('admin.category.index') }}">Category</a></li>
-                    <li class=""><a class="nav-link" href="{{ route('admin.sub-category.index') }}">Sub
-                            Category</a></li>
-                    <li class=""> <a class="nav-link" href="{{ route('admin.child-category.index') }}">Child
-                            Category</a></li>
+                    <li class="{{ setActive(['admin.category.*']) }}"><a class="nav-link"
+                            href="{{ route('admin.category.index') }}">Category</a></li>
+                    <li class="{{ setActive(['admin.sub-category.*']) }}"><a class="nav-link"
+                            href="{{ route('admin.sub-category.index') }}">Sub Category</a></li>
+                    <li class="{{ setActive(['admin.child-category.*']) }}"> <a class="nav-link"
+                            href="{{ route('admin.child-category.index') }}">Child Category</a></li>
                 </ul>
             </li>
 
@@ -31,7 +32,8 @@
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
                     <span>Manage Website</span></a>
                 <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="{{ route('admin.slider.index') }}">Slider</a></li>
+                    <li class="{{ setActive(['admin.slider.*']) }}"><a class="nav-link"
+                            href="{{ route('admin.slider.index') }}">Slider</a></li>
 
                 </ul>
             </li>
