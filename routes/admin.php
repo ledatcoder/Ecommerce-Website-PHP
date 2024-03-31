594035e76e5d17fa0fde9962c20eb8e3378bd05f
Lê Thành Đạt <datthichlaptrinh@gmail.com>
2024-03-31 23:18:35 +0700

    Ecommerce Category, Subcategory and Child Category Setup

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\SliderController;
+use App\Http\Controllers\Backend\CategoryController;
+use App\Http\Controllers\Backend\SubCategoryController;
+use App\Http\Controllers\Backend\ChildCategoryController;
 /** Admin Routes */
 
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashbaord');
@@ -14,3 +17,14 @@ Route::post('profile/update/password', [ProfileController::class, 'updatePasswor
 
 /** Slider Route */
 Route::resource('slider', SliderController::class);
+
+/** Category Route */
+Route::put('change-status', [CategoryController::class, 'changeStatus'])->name('category.change-status');
+Route::resource('category', CategoryController::class);
+/** Sub Category Route */
+Route::put('subcategory/change-status', [SubCategoryController::class, 'changeStatus'])->name('sub-category.change-status');
+Route::resource('sub-category', SubCategoryController::class);
+/** Child Category Route */
+Route::put('child-category/change-status', [ChildCategoryController::class, 'changeStatus'])->name('child-category.change-status');
+Route::get('get-subcategories', [ChildCategoryController::class, 'getSubCategories'])->name('get-subcategories');
+Route::resource('child-category', ChildCategoryController::class);
