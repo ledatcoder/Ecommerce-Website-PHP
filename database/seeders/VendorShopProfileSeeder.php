c6a0c49c9100046472c0a511014ae7d57d294dcd
Lê Thành Đạt <datthichlaptrinh@gmail.com>
2024-04-06 23:09:37 +0700

    Ecommerce Vendor Product And Related Features

--- database/seeders/VendorShopProfileSeeder.php
+++ database/seeders/VendorShopProfileSeeder.php
@@ -7,23 +7,25 @@ use App\Models\Vendor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class AdminProfileSeeder extends Seeder
+class VendorShopProfileSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $user = User::where('email', 'admin@gmail.com')->first();
+        $user = User::where('email', 'vendor@gmail.com')->first();
 
         $vendor = new Vendor();
         $vendor->banner = 'uploads/1343.jpg';
-        $vendor->shop_name = 'Admin Shop';
+        $vendor->shop_name = 'Vendor Shop';
         $vendor->phone = '12321312';
-        $vendor->email = 'admin@gmail.com';
+        $vendor->email = 'vendor@gmail.com';
         $vendor->address = 'Usa';
         $vendor->description = 'shop description';
         $vendor->user_id = $user->id;
+        $vendor->status = 1;
+
         $vendor->save();
     }
 }
--- resources/views/vendor/layouts/sidebar.blade.php
+++ resources/views/vendor/layouts/sidebar.blade.php
@@ -6,21 +6,21 @@
     </span>
     {{-- <a href="javascript:;" class="dash_logo"><img src="{{asset($logoSetting->logo)}}" alt="logo" class="img-fluid"></a> --}}
     <ul class="dashboard_link">
-      {{-- <li><a class="{{setActive(['vendor.dashbaord'])}}" href="{{route('vendor.dashbaord')}}"><i class="fas fa-tachometer"></i>Dashboard</a></li>
-      <li><a class="" href="{{route('home')}}"><i class="fas fa-home"></i>Go To Home</a></li> --}}
+      <li><a class="{{setActive(['vendor.dashbaord'])}}" href="{{route('vendor.dashbaord')}}"><i class="fas fa-tachometer"></i>Dashboard</a></li>
+      <li><a class="" href="{{route('home')}}"><i class="fas fa-home"></i>Go To Home</a></li>
 
       {{-- <li><a class="" href="{{route('vendor.messages.index')}}"><i class="fas fa-home"></i>Messenger</a></li> --}}
 
 
       {{-- <li><a class="{{setActive(['vendor.orders.*'])}}" href="{{route('vendor.orders.index')}}"><i class="fas fa-box"></i> Orders</a></li> --}}
-{{--
-      <li><a class="{{setActive(['vendor.products.*'])}}" href="{{route('vendor.products.index')}}"><i class="fas fa-cart-plus"></i> Products</a></li> --}}
+
+      <li><a class="{{setActive(['vendor.products.*'])}}" href="{{route('vendor.products.index')}}"><i class="fas fa-cart-plus"></i> Products</a></li>
       {{-- <li><a class="{{setActive(['vendor.reviews.index'])}}" href="{{route('vendor.reviews.index')}}"><i class="fas fa-star"></i> Review</a></li> --}}
 
         {{-- <li><a class="{{setActive(['vendor.reviews.index'])}}" href="{{route('vendor.withdraw.index')}}"><i class="fas fa-star"></i> My Withdraw</a></li> --}}
 
-      {{-- <li><a class="{{setActive(['vendor.shop-profile.index'])}}" href="{{route('vendor.shop-profile.index')}}"><i class="far fa-user"></i> Shop Profile</a></li>
-      <li><a class="{{setActive(['vendor.profile'])}}" href="{{route('vendor.profile')}}"><i class="far fa-user"></i> My Profile</a></li><li> --}}
+      <li><a class="{{setActive(['vendor.shop-profile.index'])}}" href="{{route('vendor.shop-profile.index')}}"><i class="far fa-user"></i> Shop Profile</a></li>
+      <li><a class="{{setActive(['vendor.profile'])}}" href="{{route('vendor.profile')}}"><i class="far fa-user"></i> My Profile</a></li><li>
 
         <form method="POST" action="{{ route('logout') }}">
             @csrf
